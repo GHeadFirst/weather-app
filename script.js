f7b8ea4b2c7197f1100eb93297ada034eaa8459b
GHeadFirst <alfaresbakri@gmail.com>
2022-06-28 18:17:26 +0300

    Added HTML, CSS and API need to fix picture to fit screen and need to fix API to work properly

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+let city;
+let lat;
+let lon;
+let units;
+// city = "Denver";
+units = "metric";
+let weather = {
+  apiKey: "fea089d984c6e8bd57973ca7c8dc4ec9",
+  fetchGeo() {
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${this.apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log((lat = data[0].lat), (lon = data[0].lon)));
+  },
+  fetchCity() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=${units}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
+};
+
+document.querySelector(".search-button").addEventListener("click", () => {
+  city = document.querySelector(".search-bar").value;
+  console.log(city);
+  weather.fetchGeo();
+  weather.fetchCity();
+});
